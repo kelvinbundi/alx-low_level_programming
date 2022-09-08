4182127031af8453ef4001eda8a3d5d502440089
kelvinbundi <kelvinkiogorakk@gmail.com>
2022-09-08 13:48:21 -0700

    my c programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** 
+/**
  * main - prints "with proper grammar, but the outcome is a piece of art,
  * Return: Always 0.
  */
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the size char,int,lomg int,long long int and float
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of char: %d byte(s)\n". sizeof(a));
+	printf("Size of int: %d byte(s)\n". sizeof(b));
+	printf("Size of long int: %d byte(s)\n". sizeof(c));
+	printf("Size of long long int: %d byte(s)\n". sizeof(d));
+	printf("Size of float: %d byte(s)\n". sizeof(e));
+	return (0)
+}
