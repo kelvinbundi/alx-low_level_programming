d4a70ae9270db286b9f38ca42e4e05310fe7e449
kelvinbundi <kelvinkiogorakk@gmail.com>
2022-09-14 14:08:17 -0700

    my nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
